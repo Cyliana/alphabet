4fbc774d7e02c83552409a176d6956a39985fc3f
Amanda TADDEI <a.taddei@codeur.online>
2021-08-30 22:25:07 +0200

    add movement

--- alpha.php
+++ alpha.php
@@ -27,9 +27,53 @@ $lettre["d"]    ="________"
                 ."_*____*_" 
                 ."_*____*_"
                 ."_*****__"
+                ."________";
+
+$lettre[" "]    ="________"
+                ."________"
+                ."________"
+                ."________"
+                ."________" 
+                ."________"
+                ."________"
                 ."________"; 
  
-$mot = "amanda";
+$mot = " amanda ";
+
+//====================== COULEURS ============================
+
+function color($c)
+{
+    switch($c)
+    {
+        case 0      : $d = "\033[30m"; break;
+        case 1      : $d = "\033[31m"; break;
+        case 2      : $d = "\033[32m"; break;
+        case 3      : $d = "\033[33m"; break;
+        case 4      : $d = "\033[34m"; break;
+        case 5      : $d = "\033[35m"; break;
+        case 6      : $d = "\033[36m"; break;
+        case 7      : $d = "\033[37m"; break;
+
+        case 'noir'     : $d = "\033[30m"; break;
+        case 'rouge'    : $d = "\033[31m"; break;
+        case 'vert'     : $d = "\033[32m"; break;
+        case 'jaune'    : $d = "\033[33m"; break;
+        case 'bleu'     : $d = "\033[34m"; break;
+        case 'magenta'  : $d = "\033[35m"; break;
+        case 'cyan'     : $d = "\033[36m"; break;
+        case 'blanc'    : $d = "\033[37m"; break;
+    }
+    print($d);
+}
+
+
+//====================== POSITIONNEMENT ============================
+
+function locateXY($x,$y)
+{
+    print("\033[".$y.";".$x."H");  
+}
 
 for($l = 0 ; $l<8 ; $l++)
 {
@@ -38,15 +82,31 @@ for($l = 0 ; $l<8 ; $l++)
     for($i = 0 ; $i<strlen($mot); $i++)
     {
         $caractere = $mot[$i];
-
         $lignes[$l] .= substr($lettre[$caractere],$l*8,8);
     }
+
+    $lignes[$l] = str_replace("_",' ',$lignes[$l])."\n";
 }
 
-for($l= 0;$l<8;$l++)
+
+$x = 4;
+$s = 1 ;
+
+while(1==1)
 {
-    print($lignes[$l]."\n");
+    for($l= 0;$l<8;$l++)
+    {
+        locateXY($x,4+$l);
+        color($l);
+        print($lignes[$l]); 
+    }
+
+    $x = $x + $s;
+    if($x ==10) $s =-1;
+    if($x == 4) $s = 1;
+    usleep(100000);
 }
+
 
 
 ?>
